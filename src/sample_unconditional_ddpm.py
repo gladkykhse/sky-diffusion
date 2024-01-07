6dd3a9d1eceaf8bac34f2777c63ed837ca62ac21
gladkykhse <gladkykh.sviatoslav@gmail.com>
2024-01-07 21:31:24 +0100

    refactoring

--- src/sample_unconditional_ddpm.py
+++ src/sample_unconditional_ddpm.py
@@ -10,37 +10,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--n_samples", default=10, type=int)
 parser.add_argument("--image_size", default=512, type=int)
 parser.add_argument("--mixed_precision", default="fp16", type=str)
-parser.add_argument(
-    "--gradient_accumulation_steps",
-    default=1,
-    type=int,
-)
-parser.add_argument(
-    "--output_dir",
-    default="unconditional_sky_diffusion",
-    type=str,
-)
+parser.add_argument("--gradient_accumulation_steps", default=1, type=int)
+parser.add_argument("--output_dir", default="unconditional_sky_diffusion", type=str)
 parser.add_argument("--samples_dir", default="samples", type=str)
 parser.add_argument("--seed", default=42, type=int)
-parser.add_argument("--model_path", type=str)
+parser.add_argument("--model_path", default="model", type=str)
 
 
-def create_model(
-    config: argparse.Namespace,
-) -> UNet2DModel:
+def create_model(config: argparse.Namespace) -> UNet2DModel:
     return UNet2DModel(
         sample_size=config.image_size,
         in_channels=3,
         out_channels=3,
         layers_per_block=2,
-        block_out_channels=(
-            128,
-            128,
-            256,
-            256,
-            512,
-            512,
-        ),
+        block_out_channels=(128, 128, 256, 256, 512, 512),
         down_block_types=(
             "DownBlock2D",
             "DownBlock2D",
@@ -66,7 +49,7 @@ def main(config: argparse.Namespace):
         device = "cuda"
 
     model = create_model(config)
-    model.load_state_dict(torch.load(config.model_path))
+    model.load_state_dict(torch.load(os.path.join(config.output_dir, config.model_path)))
     model.to(device)
 
     accelerator = Accelerator(
@@ -76,10 +59,7 @@ def main(config: argparse.Namespace):
         project_dir=os.path.join(config.output_dir, "logs"),
     )
     noise_scheduler = DDPMScheduler(num_train_timesteps=1000)
-    pipeline = DDPMPipeline(
-        unet=accelerator.unwrap_model(model),
-        scheduler=noise_scheduler,
-    )
+    pipeline = DDPMPipeline(unet=accelerator.unwrap_model(model), scheduler=noise_scheduler)
 
     images = pipeline(
         batch_size=config.n_samples,
